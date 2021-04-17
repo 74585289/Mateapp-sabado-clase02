ebcdb2cf95b0d74198b631d0930ff23286c7558c
74585289 <74585289@certus.edu.pe>
2021-04-17 11:14:49 -0500

    Update holamundo.py

---
+++ holamundo.py
@@ -0,0 +1,65 @@
+print("Hola mundo")
+edad = 20
+print(edad)
+print("tengo" ,edad, "años")
+
+base = 10
+height = 5
+area = base * height / 2
+
+print("el area es: ", area)
+
+#esto es un comentario
+#suma
+
+a = 5
+b = 6
+
+suma = a + b
+print("la suma es:" , suma)
+
+#resta
+resta = a - b
+
+print("la resta es: ", resta)
+
+#multiplicacion
+multiplicar = a * b
+print("la multiplicacion es: ", multiplicar)
+
+#dividir
+division = a / b
+print("la division es: ", division)
+
+#potencia
+potencia = a ** b
+print("la potencia es: ", potencia)
+
+#operaciones semi complejas
+#realizar la siguiente operacion en python: ((4+5)*3)/2
+
+x = 4
+y = 5
+z = 3
+w = 2
+
+result = ((x + y)*z)/w
+print(result)
+
+#operaciones con STRING
+str1 = "a"
+str2 = "b"
+
+resultado = str1 + str2
+print(resultado)
+
+print(resultado * 5)
+
+caracteres = len("hola mundo")
+
+print(caracteres)
+
+name = input("Como te llamas?") 
+
+print("Hola", name)
+print("hola" + name)
